912250623b941bc4aedcfe9cf04bcbb3010bd93c
RudranilMaventic <rudranil@maventic.com>
2026-06-10 11:52:18 +0000

    10/06/26 pratice changes

--- webapp/controller/View1.controller.js
+++ webapp/controller/View1.controller.js
@@ -1,10 +1,11 @@
 sap.ui.define([
     "sap/ui/core/mvc/Controller",
     "demofiori/project1/formatter/formatter",
-    "sap/m/MessageToast"
-    
+    "sap/m/MessageToast",
+    "sap/ui/core/Fragment"
 
-], (Controller, formatter) => {
+
+], (Controller, formatter, MessageToast, Fragment) => {
     "use strict";
 
     return Controller.extend("demofiori.project1.controller.View1", {
@@ -13,43 +14,60 @@ sap.ui.define([
 
         onInit() {
 
-            
+
 
             // Model defination in a controler
+            // let oData = {
+            // name: "Rudra",
+            // status: "Active",
+            // state: true,
+            // output: "",
+            // Employee: [
+            //     {
+            //         name: "Employee 1",
+            //         role: "Developer",
+            //         type: "Active",
+            //         joindate: "2022-01-15"
+            //     },
+
+            //     {
+            //         name: "Employee 2",
+            //         role: "Designer",
+            //         type: "Inactive",
+            //         joindate: "2022-05-15"
+            //     },
+
+            //      {
+            //         name: "Employee 3",
+            //         role: "Manager",
+            //         type: "Active",
+            //         joindate: "2022-07-15"
+            //     }
+
+            // ]
+
+
+            // }
+
+            // let oModel = new sap.ui.model.json.JSONModel(oData);
+
+            // this.getView().setModel(oModel, "employeeModel");
+
             let oData = {
-                name: "Rudra",
-                status: "Active",
-                state: true,
-                output: "",
-                Employee: [
-                    {
-                        name: "Employee 1",
-                        role: "Developer",
-                        type: "Active",
-                        joindate: "2022-01-15"
-                    },
-
-                    {
-                        name: "Employee 2",
-                        role: "Designer",
-                        type: "Inactive",
-                        joindate: "2022-05-15"
-                    },
-
-                     {
-                        name: "Employee 3",
-                        role: "Manager",
-                        type: "Active",
-                        joindate: "2022-07-15"
-                    }
-
-                ]
+                firstName: "Rudranil",
+                lastName: "Guchhait",
+                amount: 2001,
             }
 
             let oModel = new sap.ui.model.json.JSONModel(oData);
 
-            this.getView().setModel(oModel, "employeeModel");
-            console.log(this.getView().getModel("employeeModel").getData());
+            this.getView().setModel(oModel, "uiModel");
+
+            // console.log("1. onInit called");
+
+
+
+            // console.log(this.getView().getModel("employeeModel").getData());
 
             // //Global Model- component.js
             // const oCompanyModel = this.getOwnerComponent().getModel("companyModel");
@@ -68,6 +86,18 @@ sap.ui.define([
             // });
 
             //             this.getView().setModel(oModel, "employeeModel");
+
+
+            //Dynamic fragment loading
+
+            // Fragment.load({
+            //     name: "demofiori.project1.fragment.form",
+            //     controller: this
+            // }).then(function (oFragment) {
+
+            //     this.byId("formContainer").addItem(oFragment);
+
+            // }.bind(this));
         },
 
         // onSubmit: function () {
@@ -122,7 +152,84 @@ sap.ui.define([
             let sMessage = oBundle.getText("saveMessage");
             MessageToast.show(sMessage + " " + name);
 
-            
-        }
+
+        },
+
+
+        // Lifecycle hooks:
+
+        // 1. onInit: Called when the controller is instantiated and its View controls (if available) are already created. Can be used to modify the View before it is displayed, to bind event handlers and do other one-time initialization.
+
+        // 2. onBeforeRendering: Called before the controller's View is re-rendered (NOT before the first rendering! onInit() is used for that one!). Can be used to unbind event handlers and do other clean-up before the View is re-rendered.
+
+        // 3. onAfterRendering: Called when the View has been rendered (so its HTML is part of the document). Can be used to manipulate the HTML after it has been rendered.
+
+        // 4. onExit: Called when the Controller is 
+        // destroyed. Use this one to free resources 
+        // and finalize activities.
+
+
+        // onBeforerending: function () {
+        //     console.log("2. onBeforeRendering called");
+
+        //     // this.byId("textName").$().css("color", "red"); //Not working because the control is not rendered yet
+        // },
+
+        // onAfterRendering: function () {
+        //     console.log("3. onAfterRendering called");
+
+        //     this.byId("textName").$().css("color", "red");
+        // },
+
+
+        onSubmit: function () {
+
+            // let sFirstName = this.byId("idFirstName").getValue();
+            // let sLastName = this.byId("idLastName").getValue();
+            // let sAmount = this.byId("idAmount").getValue();
+
+            // sap.m.MessageToast.show(
+            //     sFirstName + " " +
+            //     sLastName + " : " +
+            //     sAmount
+            // );
+
+            MessageToast.show("Form submitted successfully!");
+
+        },
+
+        // openDialog: async function () {
+
+        //     if (!this._oDialog) {
+
+        //         this._oDialog = await Fragment.load({
+        //             name: "demofiori.project1.fragment.form",
+        //             controller: this
+        //         });
+
+        //         this.getView().addDependent(this._oDialog);
+        //     }
+
+        //     this._oDialog.open();
+        // },
+
+        onCancel: function () {
+            this._oDialog.close();
+        },
+
+        openDialog: async function (oEvent) {
+
+            if (!this._oPopover) {
+
+                this._oPopover = await Fragment.load({
+                    name: "demofiori.project1.fragment.popover",
+                    controller: this
+                });
+
+                this.getView().addDependent(this._oPopover);
+            }
+
+            this._oPopover.openBy(oEvent.getSource());
+        },
     });
 });
